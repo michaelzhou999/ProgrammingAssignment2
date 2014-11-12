766acd572ef14c5fc367e3ee93ad1b918f520ed5
Michael Zhou <michael.zhou@gmail.com>
2014-11-11 19:55:10 -0500

    init commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+library(fBasics)
 
-## Write a short comment describing this function
-
+## Take a matrix and return a list of functions
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(i) i <<- i
+    getinv <- function() i
+    list(set = set, get = get, getinv = getinv, setinv = setinv)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse of matrix, out of cache if
+## it's already calculated.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- inv(data)
+    x$setinv(m)
+    m
 }
